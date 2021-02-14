077c62f10ffb828fed88a72c0787c672e3ca1668
ilgazyuksel <ilgazyuksel@gmail.com>
2021-02-14 18:47:32 +0300

    add read and write with meta methods

--- scripts/utils.py
+++ scripts/utils.py
@@ -31,3 +31,35 @@ def apply_schema(df: DataFrame, schema: Dict) -> DataFrame:
         select_cols.append(col_name)
     df = df.select(select_cols)
     return df
+
+
+def read_with_meta(spark, df_meta: dict, *args) -> DataFrame:
+    """
+    """
+    df = None
+    path = df_meta["path"]
+    schema = df_meta["schema"]
+    data_format = df_meta["data_format"]
+    if data_format == "parquet":
+        df = spark.read.parquet(path)
+    elif data_format == "csv":
+        df = spark.read.csv(path, *args)
+    else:
+        raise AttributeError("Only csv or parquet data formats are readable")
+    df = apply_schema(df, schema=schema)
+    return df
+
+
+def write_with_meta(df, df_meta: dict):
+    """
+    """
+    path = df_meta["path"]
+    schema = df_meta["schema"]
+    try:
+        partition_cols = df_meta["partition_cols"]
+    except:
+        partition_cols = None
+        repartition_number = 3
+        df = df.repartition(repartition_number)
+    df = apply_schema(df, schema=schema)
+    df.write.parquet(path=path, mode='overwrite', partitionBy=partition_cols)
