286644c117b03279a0dc6edd68d998c467da894c
ilgazyuksel <ilgazyuksel@gmail.com>
2021-02-14 18:47:08 +0300

    add apply schema method

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,9 +1,12 @@
 """
 Doc
 """
+from typing import Dict
 
 from omegaconf import OmegaConf
+from pyspark.sql import DataFrame
 from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
 
 
 def create_spark_session() -> SparkSession:
@@ -17,3 +20,14 @@ def provide_config(path):
     conf = OmegaConf.load(path)
     resolved = OmegaConf.to_container(conf, resolve=True)
     return resolved
+
+
+def apply_schema(df: DataFrame, schema: Dict) -> DataFrame:
+    """
+    """
+    select_cols = []
+    for col_name, col_type in schema.items():
+        df = df.withColumn(col_name, F.col(col_name).cast(col_type))
+        select_cols.append(col_name)
+    df = df.select(select_cols)
+    return df
